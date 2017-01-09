634f1636b12e1a1af17aa96b42184ac555a15f3c
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-09 17:28:42 -0200

    Operadores de Atribuição(+=, -=, *=, /=, %= e .=)

---
+++ 8.Operador-De-Atribuicao.php
@@ -0,0 +1,27 @@
+<?php
+
+/*Operadores de Atribuição*/
+
+#echo "<br>";
+
+$a = 50;
+$b = 20;
+#$c = $a + $b;
+#$c = $a - $b;
+#$c = $a * $b;
+#$c = $a / $b;
+
+#print "$c";
+
+/*Operadores de Atribuição, pode ser assim: */
+#$a += $b;
+#$a -= $b;
+#$a *= $b;
+#$a /= $b;
+#$a %= $b;
+$a .= $b;
+
+print "$a";
+
+
+?>
