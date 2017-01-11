414f7a66933bbd5f833bef62de24a9347009e74c
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-11 15:11:29 -0200

    Usando if, ifelse para teste condicional

---
+++ 10.Estruturas-If-Switch.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 7;
+
+/*
+	if ($a >= 15)
+		print "O valor de a é maior que 15! <br>";
+	else
+		print "O valor de a é menor que 15! <br>";
+*/
+
+	if ($a >= 15) {
+		print "Estudando PHP <br>";
+		print "O valor de a é maior que 15! <br>";
+	}
+	elseif ($a >= 8) {
+		print "Estudando PHP <br>";
+		print "O valor de a é maior que 8! <br>";
+	}
+	else {
+		print "Estudando PHP <br>";
+		print "O valor de a é menor que 8! <br>";
+	}
+
+?>
