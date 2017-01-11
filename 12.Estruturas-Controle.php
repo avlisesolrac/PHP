a9409bd3d1293e08993ab50922c0244e10dbb2e6
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-11 15:38:01 -0200

    Estruturas de Controle while, do...while e for

---
+++ 12.Estruturas-Controle.php
@@ -0,0 +1,27 @@
+<?php
+
+//$a = 1;
+
+/*
+	while($a <= 8) {
+		print "O valor de \$a é: $a <br>";
+		$a++;
+	}
+*/
+
+/*
+	do {
+
+		print "O valor de \$a é: $a <br>";
+		$a++;
+
+	}while($a <= 8);
+*/
+
+/*
+	for($a = 1; $a <= 8; $a++) {
+		print "O valor de \$a é: $a <br>";
+	}
+*/
+	
+?>
