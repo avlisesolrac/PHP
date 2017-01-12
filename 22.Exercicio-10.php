d9cb4ef1ec5769055876aeaab7ba925e3a2a94c9
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-12 16:36:39 -0200

    Criado o arquivo 'biblioteca-php.fcn' para criar função de tratamento da função date()

---
+++ 22.Exercicio-10.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lan="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<title>Exercício - Imprimir a tabuada de um determinado número</title>
+		<?php include("biblioteca-php.fcn"); ?>
+	</head>
+	<body>
+		
+		<h2 style="text-align: center">carloseduardo.site11.com</h2>
+		
+		<!--Essa função data() está dentro do arquivo 'biblioteca-php.fcn'-->
+		<?= traduzirDate();?>
+<!--
+date()
+d - Dia do mês (01 - 31)
+D - Retorna os três caracteres que representam o dia da semana (Mon-Sun)
+F - Nome completo do mês (January - December)
+g - retorna a hora no formato de 12 horas sem começar com 0 (1-12)
+G - retorna a hora no formato 24 horas sem começar com 0 (1-23)
+h - retorna a hora no formato 12 horas com zero (01-12)
+H - retorna a hora no formato 24 horas com zero (01-23)
+i - retorna os minutos começando por zero (00-59)
+I - 1 - se for dia , zero se for noite (0,1)
+j - dia do mês sem o zero (1-31)
+l - retorna Nome o dia da semana completo
+m - retorna o número do mês começando com zero (01-12)
+M - três caracteres do nome do Mês (Jan - Dec)
+n - retorna o número do mês sem o zero
+s - Segundos com o zero (00 - 59)
+t - a quantidade de dias do mês atual
+w - retorna o número do dia da semana (0-6)
+Y - retorna o ano com quatro dígitos
+y - retorna o ano com dois dígitos
+z - o dia atual do ano (162-366)
+-->
+
+		
+
+	</body>
+</html>
