7cf6d7412aad4e7eded9694295d5b799b26faf8c
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-13 13:12:33 -0200

    Layout definido com tabelas

---
+++ cadastro/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<title> Página Principal - Curso Completo de PHP </title>
+</head>
+<body>
+
+<table width="100%" border="1" cellspacing="0" cellpadding="0">
+	<tr>
+		<td><table width="100%" border="1" cellspacing="0" cellpadding="0">
+				<tr>
+					<td colspan="2"> Cabeçalho </td>
+				</tr>
+				<tr>
+					<td width="10%">
+						<table width="100%" border="1" cellspacing="0" cellpadding="0">
+								<tr><td>Menu de opções</td></tr>
+								<tr><td>Home</td></tr>
+								<tr><td>Inserir</td></tr>
+								<tr><td>Alterar</td></tr>
+								<tr><td>Excluir</td></tr>
+								<tr><td>Consultar</td></tr>
+						</table>
+					 </td>
+					<td> 
+						<table width="100%" border="1" cellspacing="0" cellpadding="0">
+							<tr> <td> Afui fica o corpo do programa</td> </tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+
+	<tr>
+		
+	</tr>
+</table>
+
+</body>
+</html>
