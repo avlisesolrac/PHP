569a4e55d6eb2388077fa611fb40a9f2573504a1
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-13 13:33:39 -0200

    Criado a tabela de clientes que será exibido no corpo do layout

---
+++ cadastro/home.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<title> Home - Dados do Banco </title>
+</head>
+<body>
+
+<table width="100%" border="1" bordercolor="#000000" cellspacing="0" align="center" cellpadding="0">
+	<tr>
+		<th>ID</th>
+		<th>NOME</th>
+		<th>ENDEREÇO</th>
+		<th>BAIRRO</th>
+		<th>CIDADE</th>
+		<th>UF</th>
+		<th>CEP</th>
+		<th>FONE</th>
+		<th>E-MAIL</th>
+		<th>RG</th>
+		<th>CPF</th>
+		<th>SEXO</th>
+	</tr>
+	
+</table>
+
+</body>
+</html>
