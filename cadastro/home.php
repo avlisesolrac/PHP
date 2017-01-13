09a6b9065bce17cf6240b764024e3d01d9657a83
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-13 14:34:27 -0200

    Realizado a conexão com o mysql e a consulta dos registros da tabela cliente

--- cadastro/home.php
+++ cadastro/home.php
@@ -1,3 +1,6 @@
+<?php 
+include ("conexao.php")
+?>
 <!doctype html>
 <html lang="pt-BR">
 <head>
@@ -20,6 +23,38 @@
 		<th>CPF</th>
 		<th>SEXO</th>
 	</tr>
+
+	<?php
+
+//Atribuido a variável $sql o comando/string SQL para selecionar todos os registros da tabela cliente
+		$sql = "SELECT * FROM cliente";
+
+//mysql_query() executa o comando que está na variável $sql
+		$query = mysql_query($sql);
+
+//Atribuido a variável $resultado um array(matriz) que é retornado pela função mysql_fetch_array() de todos os registros da consulta realizada com a variável $query.
+//poderia usar também o mysql_result(),
+		while ($resultado = mysql_fetch_array($query)) {
+			print "
+			<tr>
+				<td>$resultado[id_cliente]</td>
+				<td>$resultado[nome]</td>
+				<td>$resultado[endereco]</td>
+				<td>$resultado[bairro]</td>
+				<td>$resultado[cidade]</td>
+				<td>$resultado[uf]</td>
+				<td>$resultado[cep]</td>
+				<td>$resultado[fone]</td>
+				<td>$resultado[email]</td>
+				<td>$resultado[rg]</td>
+				<td>$resultado[cpf]</td>
+				<td>$resultado[sexo]</td>
+			</tr>
+			";
+		}
+
+
+	?>
 	
 </table>
 
