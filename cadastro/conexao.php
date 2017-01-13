8662243470e189284a00c657d39b32ff66440f56
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-13 13:25:22 -0200

    Criado a conexão com o banco mysql

---
+++ cadastro/conexao.php
@@ -0,0 +1,19 @@
+<?php
+//Servidor do banco de dados
+$host = "localhost";
+
+//Usuário do banco default "root"
+$usuario = "root";
+
+//Senha do banco default ""
+$senha = "GtU*k101";
+
+//mysql_connect() conecta com o banco
+//or die - vai apresentar uma mensagem caso não tenha sido possível fazer a conexão
+$banco = mysql_connect($host, $usuario, $senha) or die ("Não foi possível fazer a conexão com o servidor de Banco de Dados!");
+
+//mysql_select_db() seleciona o schema da conexão feita com o mysql_connet()
+//or die - exibe uma mensagem caso não seja possível selecionar o banco especificado na função mysql_select_db()
+mysql_select_db("aulaphp", $banco) or die ("Não foi possível encontrar o schema especificado!")
+
+?>
