f6b239af9320e0b6851779cdbab521aac50c1c82
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-14 00:24:26 -0200

    Criado o formulário para Exclusão de algum cliente através do envio do comando DELETE para o mysql

--- cadastro/excluir.php
+++ cadastro/excluir.php
@@ -9,9 +9,7 @@ $sql = "DELETE FROM cliente WHERE id_cliente = '$id_cliente'";
 //Executa o comando de inserção dos dados do formulário no banco
 $query = mysql_query($sql) or die ("Houve um erro na exclusão dos dados, verifique os valores passados!");
 
-//print "Dados gravados com sucesso.";
-
-//Quando os dados forem gravados vai direto para index.php atribuindo a variavel link o indice 1, o que faz com que vá direto para a tela de home, onde são apresentados os dados do banco.
+//Quando os dados forem excluidos vai direto para index.php atribuindo a variavel link o indice 1, o que faz com que vá direto para a tela de home, onde são apresentados os dados do banco.
 header("location:index.php?link=1");
 
 ?>
