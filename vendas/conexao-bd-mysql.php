3da0173de0a7e3b1ea4fe99be6be08e548239abb
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-15 16:20:17 -0200

    Criado a conexão com o banco de dados e a tela de CRUD dos clientes

--- vendas/conexao-bd-mysql.php
+++ vendas/conexao-bd-mysql.php
@@ -1,7 +1,7 @@
 <?php
 $host 		= "localhost";
 $usuario 	= "root";
-$senha 	= "GtU*k101";
+$senha 		= "GtU*k101";
 $conexao	= mysql_connect($host, $usuario, $senha) or die ("<strong style='color:red;'>Não foi possível realizar a conexão com o BD!</strong>");
 
 mysql_select_db("controle_vendas", $conexao) or die ("<strong style='color:red;'>Não foi possível encontrar o schema, verifique o nome do banco de dados!</strong>");
